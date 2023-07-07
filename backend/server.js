6ffde233f0a06600a172d962aebbc7952bc7929b
ssobehtmo <writetoom2003@gmail.com>
2023-07-07 15:32:11 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://verdant-lolly-234618.netlify.app/",
     // credentials: true,
   },
 });
